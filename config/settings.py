343a221f138173b3c162628843f7d5ba036540b8
unknown <muhammadtoychiboyev1@gmail.com>
2023-11-15 20:46:47 +0500

    change

--- config/settings.py
+++ config/settings.py
@@ -85,12 +85,20 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'shop',
-        # "USER": '',
-        # "PASSWORD": 'muhammad571',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'dbtest',
+#         'USER': 'postgres',
+#         'PASSWORD': 'muhammad571',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
