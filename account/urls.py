653ae432b8fb691c31acb140a26a985b8670a207
unknown <muhammadtoychiboyev1@gmail.com>
2023-12-05 17:14:26 +0500

    auth

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_page, name='logout'),
     path('sign/', views.sign, name='sign'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,19 +1,38 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from .forms import AccountCreationForm, AccountLoginForm
+from django.contrib.auth import authenticate, logout, login
 
 
-def login(request):
-    form = AccountLoginForm()
-    if request.method == 'POST':
-        form = AccountLoginForm(data=request.POST)
-        if form.is_valid():
-            form.save()
+def login_page(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        print(1111111, name, password)
+        user = authenticate(request, name=name, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login O'xshadi")
+            return redirect('index')
+        else:
+            messages.error(request, "Login o'xshamadi")
             return redirect('/')
-
     ctx = {
-        'form': form,
+        # 'form': form,
     }
     return render(request, 'account/login.html', ctx)
+
+
+def logout_page(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('index')
+    # if request.user.is_authenticated:
+    #     logout(request)
+    #     print("Logout bo'ldi")
+    #     messages.success(request, "Logout o'xshadi")
+    #     return redirect('index')
+    return render(request, 'account/logout.html')
 
 
 def sign(request):
